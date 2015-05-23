b924440e901e8cb8dd081e6878d6c04b3c3d1aea
PingHanTsai <pinghan.tsai@gmail.com>
2015-05-22 21:37:19 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache. 
+## Computing the inverse of a square matrix can be done with the solve function in R. 
+
+## For example, if X is a square invertible matrix, then solve(X) 
+## returns its inverse.
+
+## makeCacheMatrix: 
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the inverse matrix
+## get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) i <<- inv
+        getinv <- function() i
+        list(set = set, get = get,
+        setinv = setinv,
+        getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve:
+## The following function calculates the inverse of the special "matrix" created with makeCacheMatrix
+## It first checks to see if the inverse has already been calculated. 
+## If so, it gets the inverse from the cache and skips the computation.
+## Otherwise, it calculates the inverse of the matrix and sets the value of the inverse in the cache via the setinv function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinv()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        matrix <- x$get()
+        i <- solve(matrix, ...)
+        x$setinv(i)
+        i
 }
